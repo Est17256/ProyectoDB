07d0976d09480d847e7a13fcf4a9fcaa4a7b5ba2
Gustavo de Leon <gdeleontobias@gmail.com>
2019-05-15 17:32:31 -0600

    funcionaaa

--- ProyectoDB/backend-api/database/migrations/2019_05_15_004549_valoropcional.php
+++ ProyectoDB/backend-api/database/migrations/2019_05_15_004549_valoropcional.php
@@ -14,11 +14,14 @@ class Valoropcional extends Migration
     public function up()
     {
         Schema::create('valoropcional', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('id_campo')->unsigned();
+            $table->engine = 'MyISAM';
+            $table->integer('id')->unsigned();
+            $table->integer('id_campo');
             $table->string('valor');
             $table->string('nombre_campo');
             $table->foreign('id_campo')->references('id')->on('opcional');
+            $table->primary(array('id', 'id_campo'));
+
         });
     }
 
